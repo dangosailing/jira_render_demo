d094881c3a941b1779b2b2ca5ea666037df9a08e
Daniel Salin <daniel.salin@gmail.com>
2025-09-09 12:50:52 +0200

    fe: Update App to make test api call to BE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL || ""}/api/user`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {data.name}</h1>
     </div>
-  )
+  );
 }
