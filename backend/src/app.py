406a3c88bc44d81f8ff5557a8b8a4239f3369965
Daniel Salin <daniel.salin@gmail.com>
2025-09-09 14:42:39 +0200

    refactor: routes in app file to use route blueprints

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,21 +1,18 @@
 import os
-from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask import Flask
 from flask_cors import CORS
+from routes.general import general_bp
+from routes.user import user_bp
 
 load_dotenv()
 
 app = Flask(__name__)
+
 CORS(app, resources={r"/*": {"origins": os.getenv("CORS_ORIGIN", "*")}})
 
-@app.route("/")
-def home():
-    username = os.getenv("USER", "fallbackDaniel")
-    return f"Welcome to the Home page {username}!"
-
-@app.get("/api/user")
-def hello():
-    return jsonify({"name": "user"})
+app.register_blueprint(general_bp)
+app.register_blueprint(user_bp)
 
 if __name__ ==  "__main__":
         app.run(host="0.0.0.0", port=5000, debug=True)
